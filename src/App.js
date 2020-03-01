b05bd901f49e8ae2915e003931a27423f065836d
231jk0 <zdjuric@yandex.com>
2020-03-01 16:35:27 +0100

    novi update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      {'Dogodio se neki update'}
-    </div>
-  );
+	return (
+		<div className="App">
+			{'Dogodio se neki update'} <br/>
+			{'Dogodio se jos neki update'} <br/>
+			<div style={{ backgroundColor: 'green', color: 'white' }}>
+				Marko voli gledati filmove
+			</div>
+		</div>
+	);
 }
 
 export default App;
